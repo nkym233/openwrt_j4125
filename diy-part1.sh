b9c75291303c192d72bae0999c8d083e1b483fc0
nkym233 <50127423+nkym233@users.noreply.github.com>
2022-11-18 10:04:59 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,6 +9,7 @@
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
+sudo apt install libtinfo5 
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
