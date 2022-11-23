9a51a2a90f3b6f39c74dcbc2fcf0f557479a8e5e
nkym233 <50127423+nkym233@users.noreply.github.com>
2022-11-23 22:19:13 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
-#
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
-#diy2.sh
-#删除lean大集成的旧版argon主题，更换为新版argon主题#Change Argon Theme
-rm -rf ./feeds/luci/themes/luci-theme-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git ./feeds/luci/themes/luci-theme-argon
-#更换默认主题为argon
-sed -i 's#luci-theme-bootstrap#luci-theme-argon#g' feeds/luci/collections/luci/Makefile
-sed -i 's/bootstrap/argon/g' feeds/luci/modules/luci-base/root/etc/config/luci
-
-# 修改argon主题默认壁纸Change default BackGround img
-wget -O ./feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg https://raw.githubusercontent.com/nkym233/img/main/r2g7rm.jpg
-# 修改默认后台ip default IP
+# 修改openwrt登陆地址 
 sed -i 's/192.168.1.1/192.168.31.88/g' package/base-files/files/bin/config_generate
+# 修改默认banner
+wget -O ./package/base-files/files/etc/banner https://raw.githubusercontent.com/nkym233/img/main/banner
+
+# 更换默认主题为argone
+git clone -b 21.02 https://github.com/kenzok78/luci-theme-argonne ./package/lean/luci-theme-argonne
+
+sed -i 's#luci-theme-bootstrap#luci-theme-argone#g' feeds/luci/collections/luci/Makefile
+sed -i 's/bootstrap/argone/g' feeds/luci/modules/luci-base/root/etc/config/luci
+
+# 修改argone主题默认壁纸Change default BackGround img
+wget -O ./package/lean/luci-theme-argonne/htdocs/luci-static/argonne/img/bg1.jpg https://raw.githubusercontent.com/nkym233/img/main/r2g7rm.jpg
+
+# 修改默认docker挂载路径
+sed -i "s/opt/mnt\/sda4/g" ./feeds/packages/utils/dockerd/files/daemon.json
