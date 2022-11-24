1668a01a69057a2e50969fd0b546b0874b82b529
nkym233 <50127423+nkym233@users.noreply.github.com>
2022-11-24 10:19:58 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -4,14 +4,13 @@ sed -i 's/192.168.1.1/192.168.31.88/g' package/base-files/files/bin/config_gener
 # 修改默认banner
 wget -O ./package/base-files/files/etc/banner https://raw.githubusercontent.com/nkym233/img/main/banner
 
-# 更换默认主题为argone
-git clone -b 21.02 https://github.com/kenzok78/luci-theme-argonne ./package/lean/luci-theme-argonne
+# 替换默认argon主题
+rm -rf package/feeds/luci/luci-theme-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/feeds/luci/luci-theme-argon
+sed -i 's#luci-theme-bootstrap#luci-theme-argon#g' feeds/luci/collections/luci/Makefile
+sed -i 's/bootstrap/argon/g' feeds/luci/modules/luci-base/root/etc/config/luci
 
-sed -i 's#luci-theme-bootstrap#luci-theme-argone#g' feeds/luci/collections/luci/Makefile
-sed -i 's/bootstrap/argone/g' feeds/luci/modules/luci-base/root/etc/config/luci
-
-# 修改argone主题默认壁纸Change default BackGround img
-wget -O ./package/lean/luci-theme-argonne/htdocs/luci-static/argonne/img/bg1.jpg https://raw.githubusercontent.com/nkym233/img/main/r2g7rm.jpg
+# 修改argon主题默认壁纸Change default BackGround img
+wget -O ./package/lean/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg https://raw.githubusercontent.com/nkym233/img/main/r2g7rm.jpg
 
 # 修改默认docker挂载路径
 sed -i "s/opt/mnt\/sda4/g" ./feeds/packages/utils/dockerd/files/daemon.json
