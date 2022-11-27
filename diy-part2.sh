945ba4b7d3a4b07c4004abb7cae5bcf64717a818
nkym233 <50127423+nkym233@users.noreply.github.com>
2022-11-27 12:37:45 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # 修改openwrt登陆地址 
-sed -i 's/192.168.1.1/192.168.31.88/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 # 修改默认banner
 wget -O ./package/base-files/files/etc/banner https://raw.githubusercontent.com/nkym233/img/main/banner
 
@@ -15,3 +15,6 @@ wget -O ./feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jp
 
 # 修改默认docker挂载路径
 sed -i "s/opt/mnt\/sda4/g" ./feeds/packages/utils/dockerd/files/daemon.json
+
+# 修改版本号
+sed -i "s/OpenWrt /nkym build $(TZ=UTC-8 date "+%Y%m%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
